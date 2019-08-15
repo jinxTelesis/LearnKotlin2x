344266ffa8bbf2e2b9c8fe8231209968fc4c8b16
Andre Lussier <Dremon7@hotmail.com>
2019-08-14 22:04:15 -0400

    Initial commit

---
+++ app/src/main/java/com/example/learnkotlin2x/yo.kt
@@ -0,0 +1,17 @@
+package com.example.learnkotlin2x
+
+
+fun main(){
+
+    val str:String
+    str = " Welcome to Kotlin "
+
+
+    var department:String?
+    department = null
+    department = "Software Eningeering"
+    print("Department ${department!!}")
+}
+
+class yo {
+}
