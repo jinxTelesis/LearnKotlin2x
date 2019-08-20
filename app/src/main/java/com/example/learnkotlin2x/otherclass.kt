4f77d9f627dccc50813d37ab82cb17a89b2fc3e8
Andre Lussier <Dremon7@hotmail.com>
2019-08-19 23:26:44 -0400

    Practice classes

---
+++ app/src/main/java/com/example/learnkotlin2x/otherclass.kt
@@ -0,0 +1,16 @@
+package com.example.learnkotlin2x
+
+class OtherClass(){
+    var string1:String?=null
+    var name:String?=null
+    var taco:Double?=null
+    var price:Double?=null
+    constructor(string1:String,name:String,taco:Double,price:Double):this(){
+        this.string1=string1
+        this.name = name
+        this.taco=taco
+        this.price=price
+    }
+
+
+}
---
+++ app/src/main/java/com/example/learnkotlin2x/thirdClass.kt
@@ -0,0 +1,21 @@
+package com.example.learnkotlin2x
+
+class Joanna(){
+    var first:String?=null
+    var last:String?=null
+    var family:String?=null
+    var brother:String?=null
+    var brother2:String?=null
+    var money:Double?=null
+
+    constructor(first:String,last:String,family:String,brother:String,brother2: String,money:Double):this(){
+        this.first = first
+        this.last = last
+        this.family = family
+        this.brother = brother
+        this.brother2 = brother2
+        this.money = money
+
+    }
+
+}
